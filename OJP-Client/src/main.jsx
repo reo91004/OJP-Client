b5d1e2d6216528c3193753cbbd940f6425dfc479
Woojin Jang <jinny9650@gmail.com>
2025-02-02 06:59:56 +0000

    feat: set react-redux

--- OJP-Client/src/main.jsx
+++ OJP-Client/src/main.jsx
@@ -1,3 +1,6 @@
+import { Provider } from 'react-redux'
+import store from './states/store.js'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,10 +8,11 @@ import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
-
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
---
+++ OJP-Client/src/states/store.js
@@ -0,0 +1,7 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+/// 이곳에 state 작성
+
+export default configureStore({
+  reducer: { }
+})
